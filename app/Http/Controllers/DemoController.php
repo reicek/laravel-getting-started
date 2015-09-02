65a820b1c6601d3785bac978a0516f629be77766
reicek <reicek@gmail.com>
2015-09-02 18:36:33 -0500

    'v1.0'

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -2,10 +2,8 @@
 
 namespace demo\Http\Controllers;
 
-class DemoController extends Controller
-{
-    public function sayHi ($parametro = 'Platzi')
-    {
+class DemoController extends Controller {
+    public function sayHi ($parametro = 'Platzi') {
         return view('bienvenida', ['parametro' => $parametro]);
     }
 }
